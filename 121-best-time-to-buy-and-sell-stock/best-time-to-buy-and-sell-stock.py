823310f5d7e0833fc05d93d56526b68584b749e9
punishermortal <51474998+punishermortal@users.noreply.github.com>
2026-04-06 21:19:22 +0530

    Time: 59 ms (35.50%) | Memory: 28.6 MB (84.38%) - LeetSync

--- 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.py
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.py
@@ -3,13 +3,7 @@ class Solution:
         profit = 0
         lower_price = 99999
         for i in range(len(prices)):
-            if prices[i]<lower_price:
-                lower_price = prices[i]
-            # lower_price = min(lower_price,prices[i])
-
-
-            if prices[i] - lower_price > profit:
-                profit = prices[i] - lower_price
-            
+            lower_price = min(lower_price,prices[i])
+            profit = max(profit , prices[i] - lower_price)
         return profit
         
