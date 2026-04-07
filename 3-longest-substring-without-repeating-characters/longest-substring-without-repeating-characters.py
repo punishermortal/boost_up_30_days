d2535d448f7c5ca8be7255e040afcf1e3ac7aa8e
punishermortal <51474998+punishermortal@users.noreply.github.com>
2026-04-07 10:07:32 +0530

    Time: 11 ms (77.01%) | Memory: 19.4 MB (17.73%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.py
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.py
@@ -1,34 +1,3 @@
-# class Solution:#abba
-#     def lengthOfLongestSubstring(self, s: str) -> int:
-#         if len(s)==0:
-#             return 0
-#         if len(s)==1:
-#             return 1
-#         longest_substring = 0
-#         curr_string = ""
-#         hm={}
-#         for i in range(len(s)):
-#             if s[i] in hm:
-#                 if hm[s[i]]>hm[curr_string[0]]:
-#                     curr_string=s[hm[s[i]]+1:i+1]
-#                 hm[s[i]]=i
-#             else:
-#                 hm[s[i]]=i
-#                 curr_string+=s[i]
-#             # print(curr_string)
-#             longest_substring =max(longest_substring,len(curr_string))
-#         # for i in range(len(s)):
-#         #     if s[i] in hm:
-#         #         longest_substring = max(longest_substring , len(curr_string))
-#         #         curr_string = s[i]
-#         #         hm.clear()
-#         #         hm[s[i]] = i
-#         #     else:
-#         #         hm[s[i]] = i
-#         #         curr_string += s[i]
-#         #     longest_substring = max(longest_substring , len(curr_string))
-#         return longest_substring
-
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
         hm = {}
